a81ea693baafe6d6fc7b4babfb3a1b4251ff8331
rnrrmr <127065717+rnrrmr@users.noreply.github.com>
2023-10-25 01:57:53 +0000

    Update sam_dash.js

--- sam_dash.js
+++ sam_dash.js
@@ -18,68 +18,22 @@ toggleButton.addEventListener("click", function() {
     }
 });
 
-<!-- CSV 파일을 읽어와서 테이블에 데이터를 추가하는 함수 -->
 function addDataToTable() {
     fetch('real_estate.csv') // CSV 파일 가져오기
         .then(response => response.text())
         .then(data => {
             // CSV 데이터를 파싱
             const rows = data.trim().split('\n').map(row => row.split(','));
-
-            // 첫 번째 행을 thead로 사용
-            const theadRow = rows.shift();
-            const tableHead = document.querySelector('#myTable thead tr');
-
-            // 데이터를 HTML 테이블의 thead에 추가
-            theadRow.forEach(cell => {
-                tableHead.innerHTML += `<th>${cell}</th>`;
-            });
-
-            // 데이터를 HTML 테이블의 tbody에 추가
+            
+            // 데이터를 HTML 테이블에 추가
             const tableBody = document.querySelector('#myTable tbody');
             rows.forEach(row => {
                 const rowData = row.map(cell => `<td>${cell}</td>`).join('');
                 tableBody.innerHTML += `<tr>${rowData}</tr>`;
             });
-
+            
             // 테이블 표시
             document.getElementById('myTable').style.display = 'table';
         })
         .catch(error => console.error(error));
 }
-
-// 페이지가 로드된 후 데이터를 추가
-window.addEventListener('load', addDataToTable);
-
-
-// function insertAfter(newNode, referenceNode) {
-//     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-// }
-
-// function chartLabel(){
-//   var _div = document.createElement('div');
-//   _div.className = 'chart-total';
-//   _div.innerHTML = `<span class="chart-total-num">Total:<br> 3,135</span>
-//                     <span class="chart-total-text1">Automobile</span>
-//                     <span class="chart-total-text2">Disablility</span>
-//                     <span class="chart-total-text3">Life</span>
-//                     <span class="chart-total-text4">Property</span>`;
-//   insertAfter(_div, _chart);
-// }
-
-// function chartDraw() {
-//     for (var i = 0; i < _chartBar.length; i++) {
-//         var _num = _chartBar[i].dataset.deg;
-//         newDeg.push(_num);
-//     }
-
-//     _chart.style.background = 'conic-gradient(' +
-//         color[0] + ' ' + newDeg[0] + 'deg, ' +
-//         color[1] + ' ' + newDeg[1] + 'deg, ' +
-//         color[2] + ' ' + newDeg[2] + 'deg, ' +
-//         color[3] + ' ' + newDeg[3] + 'deg )';
-
-//     chartLabel();
-// }
-
-// chartDraw();
